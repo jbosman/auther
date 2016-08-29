8d43641a65628a0fab17c3515b8a5fe6420366a8
Joe Bosman <joseph.a.bosman@gmail.com>
2016-08-29 15:45:43 -0400

    Up to 2 player

--- starting-point/browser/js/player/player.controller.js
+++ starting-point/browser/js/player/player.controller.js
@@ -1,21 +1,51 @@
 /* global juke */
 'use strict';
 
-juke.controller('PlayerCtrl', function ($scope, $rootScope, PlayerFactory) {
+// juke.controller('PlayerCtrl', function ($scope, $rootScope, PlayerFactory) {
 
-  angular.extend($scope, PlayerFactory); // copy props from param2 to param1
+//   angular.extend($scope, PlayerFactory); // copy props from param2 to param1
 
-  $scope.toggle = function () {
-    if ( PlayerFactory.isPlaying() ) PlayerFactory.pause();
-    else PlayerFactory.resume();
+//   $scope.toggle = function () {
+//     if ( PlayerFactory.isPlaying() ) PlayerFactory.pause();
+//     else PlayerFactory.resume();
+//   };
+
+//   $scope.getPercent = function () {
+//     return PlayerFactory.getProgress() * 100;
+//   };
+
+//   $scope.handleProgressClick = function (evt) {
+//     PlayerFactory.seek(evt.offsetX / evt.currentTarget.scrollWidth);
+//   };
+
+// });
+
+juke.directive('myPlayer', function(PlayerFactory){
+  var obj = {};
+
+  console.log("myPlayer directive")
+
+  obj.restrict = 'E';
+  obj.templateUrl = '/js/player/template/player.html'
+  // obj.scope = {
+  //   data: '='
+  // }
+  obj.link = function(scope){
+    angular.extend(scope, PlayerFactory);
+
+    scope.toggle = function () {
+      if ( PlayerFactory.isPlaying() ) PlayerFactory.pause();
+      else PlayerFactory.resume();
+    };
+
+    scope.getPercent = function () {
+      return PlayerFactory.getProgress() * 100;
+    };
+
+    scope.handleProgressClick = function (evt) {
+      PlayerFactory.seek(evt.offsetX / evt.currentTarget.scrollWidth);
+    };
   };
 
-  $scope.getPercent = function () {
-    return PlayerFactory.getProgress() * 100;
-  };
-
-  $scope.handleProgressClick = function (evt) {
-    PlayerFactory.seek(evt.offsetX / evt.currentTarget.scrollWidth);
-  };
-
+  return obj;
 });
--- starting-point/browser/js/sidebar/sidebar.controller.js
+++ starting-point/browser/js/sidebar/sidebar.controller.js
@@ -5,3 +5,16 @@ juke.controller('SidebarCtrl', function ($scope) {
   // nothing to see here for now… state transitions happening with ui-sref!
 
 });
+
+juke.directive('mySidebar', function(){
+	var obj = {}
+
+	obj.restrict = "E";
+
+	obj.templateUrl = "js/sidebar/template/sidebar.html";
+	//obj.templateUrl = "/template/sidebar.html";
+
+	obj.scope;
+
+	return obj;
+})
